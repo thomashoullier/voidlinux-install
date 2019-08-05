3d0b47397a1e4a79af74616946f4f0b792c9ac15
Karl <x>
2019-08-05 15:23:32 +0200

    more system configuration

--- base-install.sh
+++ base-install.sh
@@ -2,6 +2,8 @@
 # Basic system installation
 # Inputs:
 # 1. Device. Eg. "/dev/sda"
+# 2. Volume groupe name. Eg. "void"
+# 3. Hostname.
 
 wget https://alpha.de.repo.voidlinux.org/static/xbps-static-latest.x86_64-musl.tar.xz
 sudo tar xf xbps-static-latest.x86_64-musl.tar.xz -C /mnt
@@ -21,8 +23,22 @@ sudo mount --rbind /dev /mnt/dev
 sudo mount --rbind /sys /mnt/sys
 
 # Get the UUID of the EFI partition.
-uuid=$(blkid -o value -s UUID "$1"1)
+uuid_efi=$(blkid -o value -s UUID "$1"1)
 
 # Configure system files
 sudo cp -f rc.conf /mnt/etc/rc.conf
 
+echo "tmpfs /tmp tmpfs defaults,nosuid,nodev 0 0" | sudo tee /mnt/etc/fstab \
+> /dev/null
+echo "UUID=$uuid_efi /boot vfat defaults 0 2" | sudo tee -a /mnt/etc/fstab \
+> /dev/null
+echo "/dev/mapper/$2-root / ext4 defaults,noatime 0 1" | sudo tee -a \
+/mnt/etc/fstab > /dev/null
+echo "/dev/mapper/$2-swap none swap defaults 0 1" | sudo tee -a /mnt/etc/fstab \
+> /dev/null
+
+echo "$3" | sudo tee /mnt/etc/hostname > /dev/null
+
+echo "hostonly=yes" | sudo tee /mnt/etc/dracut.conf.d/hostonly.conf > /dev/null
+
+sudo cp -f grub /mnt/etc/default/grub
